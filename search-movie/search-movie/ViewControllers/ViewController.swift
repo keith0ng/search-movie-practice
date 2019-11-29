adab09ef646bd2b0f53d29cbb9dbb10d2f014621
Keith Samson <keithmark_samson@yahoo.com>
2019-11-29 14:53:38 +0800

    Initial project commit.

---
+++ search-movie/search-movie/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  search-movie
+//
+//  Created by Keith Samson on 11/29/19.
+//  Copyright © 2019 Keith Samson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
