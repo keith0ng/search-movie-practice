50672c1452aa6a4ca2d6955ed3388c28a8b41ac2
Keith Samson <keithmark_samson@yahoo.com>
2019-12-02 15:37:24 +0800

    Added selecting recent search will push to the movie results list.

--- search-movie/search-movie/ViewControllers/SearchViewController.swift
+++ search-movie/search-movie/ViewControllers/SearchViewController.swift
@@ -20,11 +20,15 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView?.delegate = self
+        
+        mainView?.tableView.delegate = self
+        mainView?.tableView.dataSource = self
     }
 }
 
 extension SearchViewController: SearchViewDelegate {
     func searchButtonTapped(searchBar: UISearchBar) {
+        mainView?.tableView.isHidden = true
         searchBar.resignFirstResponder()
         let movieListVC = MovieListViewController()
         movieListVC.searchKeyword = searchBar.text?.lowercased()
@@ -32,10 +36,35 @@ extension SearchViewController: SearchViewDelegate {
     }
     
     func searchBarDidBeginEditing(searchBar: UISearchBar) {
-        
+        mainView?.tableView.isHidden = false
     }
     
     func searchBarCencelButtonTapped(searchBar: UISearchBar) {
+        mainView?.tableView.isHidden = true
         searchBar.resignFirstResponder()
     }
 }
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        mainView?.movieSearchBar.resignFirstResponder()        
+        let movieListVC = MovieListViewController()
+        movieListVC.searchKeyword = ""
+        navigationController?.pushViewController(movieListVC, animated: true)
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellReuseIdentifier")
+        let row = indexPath.row
+        cell.textLabel?.text = "Hello, world."
+        return cell
+    }
+}
+
+
